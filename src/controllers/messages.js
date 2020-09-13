2a544bbc67f3f2f05fd2a62fbeb43683323746b2
Yuri Oliveira <soaresyuri@gmail.com>
2020-09-13 15:01:00 -0300

    feat: add yup to validate routes

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,26 +1,35 @@
 const Router = require('@koa/router')
 const { StatusCodes } = require('http-status-codes')
+const yup = require('yup')
+const validate = require('koa-yup-validator')
 const queue = require('../services/queue')
 
 const router = new Router({ prefix: '/messages' })
 
-router.post('/write', async (ctx) => {
-  const { message } = ctx.request.body
-  const messageId = await queue.append(message)
+const schemas = {
+  limit: yup.number().positive().default(1),
+  write: yup.object().shape({
+    message: yup.string().max(2**53 - 1).required(),
+  }),
+  done: yup.array().of(yup.string()).min(1).required()
+}
 
-  ctx.body = messageId
-})
-
-router.get('/', async (ctx) => {
-  const limitQuery = Number(ctx.request.query.limit)
-  const limit = Number.isNaN(limitQuery) ? 1 : limitQuery
+router.get('/', validate(schemas.limit, { path: 'request.query.limit' }), async (ctx) => {
+  const { limit } = ctx.request.query
 
   const messagesToProcess = await queue.getAvailableMessages(limit)
 
   ctx.body = messagesToProcess
 })
 
-router.post('/done', async (ctx) => {
+router.post('/write', validate(schemas.write), async (ctx) => {
+  const { message } = ctx.request.body
+  const messageId = await queue.append(message)
+
+  ctx.body = messageId
+})
+
+router.post('/done', validate(schemas.done) ,async (ctx) => {
   const idsDone = ctx.request.body
   queue.markProcessed(idsDone)
 
