23ee12488cc4b7bd16a49120995a3e49aff68124
Yuri Oliveira <soaresyuri@gmail.com>
2020-09-13 23:31:02 -0300

    feat: add helmet and cors
    To better be able to deploy.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config()
 const Koa = require('koa');
+const helmet = require('koa-helmet')
+const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
 const messages = require('./src/controllers/messages')
 
 const app = new Koa()
 
+app.use(helmet());
+app.use(cors({ origin: process.env.CORS, optionsSuccessStatus: 200 }));
 app.use(bodyParser())
 app
   .use(messages.routes())
